f4233b0cd1494eef1a48377aa08438f09eac8fbe
Androz <androz2091@gmail.com>
2021-12-26 19:05:27 +0100

    :sparkles: Create db

---
+++ app/create-db.js
@@ -0,0 +1,10 @@
+const {createConnection} = require("typeorm");
+
+const connection = await createConnection({
+    type: 'postgres',
+    host: `${process.env.P_NAME}_postgres_1`,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+}).then(async connection => {
+    connection.query('CREATE DATABASE IF NOT EXISTS "vinted_bot"');
+});
